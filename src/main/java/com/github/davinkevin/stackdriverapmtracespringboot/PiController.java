98f09abab59a821c8efa92042baa55fc3be37e8c
davinkevin <davin.kevin@gmail.com>
2018-07-13 21:55:25 +0200

    feat(reactive): move to reactive web module

--- src/main/java/com/github/davinkevin/stackdriverapmtracespringboot/PiController.java
+++ src/main/java/com/github/davinkevin/stackdriverapmtracespringboot/PiController.java
@@ -2,14 +2,15 @@ package com.github.davinkevin.stackdriverapmtracespringboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class PiController {
     @GetMapping("pi")
-    public Double pi() throws InterruptedException {
-
-        Thread.sleep(1000);
-
-        return 3.14;
+    public Mono<Double> pi(){
+        return Mono.just(3.14)
+                .delayElement(Duration.ofSeconds(1));
     }
 }
